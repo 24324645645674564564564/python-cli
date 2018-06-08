e08f75535ba528fe952534f95dee0aba985eaeb1
Scotabasa <max.gnedenko06@gmail.com>
2018-06-08 14:55:11 +0300

    Update main.py

--- main.py
+++ main.py
@@ -6,37 +6,37 @@ class Cli(cmd.Cmd):
     def __init__(self):
         cmd.Cmd.__init__(self)
         self.prompt = "> "
-        self.intro  = "Добро пожаловать\nДля справки наберите 'help'"
-        self.doc_header ="Доступные команды (для справки по конкретной команде наберите 'help _команда_')"
+        self.intro  = "Welcome\n for the details enter help 'help'"
+        self.doc_header ="Availbale commands "
 
     def do_show_cpu(self, args):
-        """show_cpu - нагрузка на процессоры"""
+        """show_cpu - cpu usage"""
         os.system("sar 2")
 
     def do_show_mem(self, args):
-        """show_mem - использование RAM"""
+        """show_mem -  RAM usage"""
         os.system("free")
 
     def do_show_disk(self, args):
-        """show_disk - свободное место на диске"""
+        """show_disk - free space on hdd"""
         os.system("df -h")
 
     def do_show_net(self, args):
-        """show_net - сетевые параметры"""
+        """show_net -net swttings"""
         os.system("sudo ifconfig")
         os.system("sudo route -n")
 
 
     def default(self, line):
-        print ("Несуществующая команда")
+        print ("error")
     def do_syslog(self,arg):
-        """syslog-системный лог """
+        """syslog-system log """
         os.system("sudo tail -f /var/log/syslog")    
     def do_browser(self,arg):
-        """ browser-запустить браузер"""
+        """ browser-star browser"""
         os.system("/usr/bin/lynx")
     def do_nload(self,arg):
-        """ загрузка сети"""
+        """ network usage """
         os.system("nload")
             
     def emptyline(self):
@@ -47,4 +47,4 @@ if __name__ == "__main__":
     try:
         cli.cmdloop()
     except KeyboardInterrupt:
-        print ("завершение сеанса...")
+        print ("Goodbye")
